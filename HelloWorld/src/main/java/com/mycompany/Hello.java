ab4342b27c0d10f2ca32bc17aa6dee210340c1e4
Chris Mitola <christophermitola@gmail.com>
2019-12-02 16:49:33 -0500

    init commit

---
+++ HelloWorld/src/main/java/com/mycompany/Hello.java
@@ -0,0 +1,18 @@
+
+/**
+ *
+ * @author chris mitola
+ */
+package com.mycompany; // Use Your Droup Id vslue here instead of com.tsg
+
+public class Hello {   
+    
+    public static void main(String[] args) {    
+        System.out.println("Hello, World!");
+        System.out.println("My name is chris");
+   }
+}
+
+
+   
+
